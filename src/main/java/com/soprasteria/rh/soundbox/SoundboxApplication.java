a69022a458f01d4ac78d24655524895deeff4766
PENNAMEN Adrien <adrien.pennamen@soprasteria.com>
2023-08-25 12:43:27 +0200

    init: spring boot starter

---
+++ src/main/java/com/soprasteria/rh/soundbox/SoundboxApplication.java
@@ -0,0 +1,13 @@
+package com.soprasteria.rh.soundbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundboxApplication.class, args);
+	}
+
+}
